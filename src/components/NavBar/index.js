a807029acf43b17b2f891a5ad88a70be4471db5e
AlexandrChebotar <44335524+AlexandrChebotar@users.noreply.github.com>
2019-06-14 01:53:17 +0300

    Add Navbar details

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,35 +1,62 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 
 import './style.sass';
 
-class App extends Component {
+class NavBar extends Component {
+  state = {
+    activeItem: '',
+  };
 
-  items = [
-    'squad',
-    'office',
-    'competitions',
-    'forum',
-    'help',
-    'settings',
-  ];
+  items = {
+    'squad': ['senior', 'youth', 'training'],
+    'office': ['finances', 'personal', 'buildings', 'calendar'],
+    'competitions': ['leagues', 'cups', 'national', 'current'],
+    'forum': ['general', 'national'],
+    'help': ['manual', 'tour'],
+    'settings': ['alternate UI', 'user', 'club'],
+  };
 
-  getNavbarItems = () => {
-    return this.items.map((item, index) => (
-      <a href="/" key={index} >
+  showDetails = (activeItem) => {
+    this.setState({activeItem});
+  };
+
+  hideDetails = () => {
+    this.setState({activeItem: ''});
+  }
+
+  getNavbarItems() {
+    return Object.keys(this.items).map((item, index) => (
+      <a href="/" key={index} onMouseEnter={() => this.showDetails(item)} >
         <svg className="navbar-item">
           <use xlinkHref={`images/icons.svg#${item}`}></use>
         </svg>
       </a>
     ));
-  };
+  }
+
+  getNavbarDetailsList() {
+    return this.items[this.state.activeItem].map((item, index) => (
+      <li key={index} >{item}</li>
+    ));
+  }
 
   render() {
     return (
-      <div className="navbar">
-        {this.getNavbarItems()}
-      </div>
+      <Fragment>
+        <div className="navbar">
+          {this.getNavbarItems()}
+        </div>
+        {this.state.activeItem &&
+          <div className="navbar-details" onMouseLeave={this.hideDetails}>
+            <h1>{this.state.activeItem}</h1>
+            <ul>
+              {this.getNavbarDetailsList()}
+            </ul>
+          </div>
+        }
+      </Fragment>
     );
   }
 }
 
-export default App;
+export default NavBar;

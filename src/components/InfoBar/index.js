66b6ba063e2c9ee112d0187fb01ac200596518df
AlexandrChebotar <44335524+AlexandrChebotar@users.noreply.github.com>
2019-06-19 00:35:08 +0300

    Refactor Infobar: changed to SFC

--- src/components/InfoBar/index.js
+++ src/components/InfoBar/index.js
@@ -15,35 +15,31 @@ const mapStateToProps = ({user, currentClub}) => {
   return {user, currentClub};
 };
 
-class Infobar extends Component {
-
-  render() {
-    const {user, currentClub} = this.props;
-    return (
-      <div className="infobar-container">
-        <Navbar className="infobar">
-          <Navbar.Group >
-            <Button href="#">
-              <H1>Tactical Football</H1>
-            </Button>
-          </Navbar.Group>
-          <Navbar.Group align="right">
-            {user ?
-              <Fragment>
-                <ClubsMenu user={user} currentClub={currentClub} />
-                <Navbar.Divider />
-                <Button href="#" text="s20 wk7/14 19:47" rightIcon="calendar" />
-              </Fragment>
-            :
-              <Text>s20 wk7/14 19:47</Text>
-            }
-            <Navbar.Divider />
-            <UserMenu user={user} />
-          </Navbar.Group>
-        </Navbar>
-      </div>
-    )
-  };
-}
+const Infobar = ({user, currentClub}) => {
+  return (
+    <div className="infobar-container">
+      <Navbar className="infobar">
+        <Navbar.Group >
+          <Button href="#">
+            <H1>Tactical Football</H1>
+          </Button>
+        </Navbar.Group>
+        <Navbar.Group align="right">
+          {user ? 
+            <Fragment>
+              <ClubsMenu user={user} currentClub={currentClub} />
+              <Navbar.Divider />
+              <Button href="#" text="s20 wk7/14 19:47" rightIcon="calendar" />
+            </Fragment>
+          :
+            <Text>s20 wk7/14 19:47</Text>
+          }
+          <Navbar.Divider />
+          <UserMenu user={user} />
+        </Navbar.Group>
+      </Navbar>
+    </div>
+  )
+};
 
 export default connect(mapStateToProps)(Infobar);

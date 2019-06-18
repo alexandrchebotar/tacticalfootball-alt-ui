9060c70e1410f46778cb830c8e64a574f04fa60c
AlexandrChebotar <44335524+AlexandrChebotar@users.noreply.github.com>
2019-06-18 23:53:59 +0300

    Add UserMenu component

---
+++ src/components/InfoBar/UserMenu/index.js
@@ -0,0 +1,44 @@
+import React, {Fragment} from 'react';
+import {
+  Menu,
+  Popover,
+  Icon,
+  Button,
+} from "@blueprintjs/core";
+
+
+const UserMenu = (user) => {
+  return (
+    <Fragment>
+      {user ?
+        <Fragment>
+          {user.messages &&
+            <Button href="#" icon="envelope" intent="warning" minimal />
+          }
+          <Popover 
+            content={(
+              <Menu>
+                <Menu.Item href="#" text="settings" icon="cog" />
+                <Menu.Item
+                  href="#"
+                  text="messages" 
+                  icon={user.messages ? <Icon icon="envelope" intent="warning" /> : "envelope"}
+                  />
+                <Menu.Divider />
+                <Menu.Item href="#" text="logout" icon="log-out" />
+              </Menu>
+            )}
+            position="bottom-right"
+            minimal
+          >
+            <Button text={user.name} rightIcon="user" />
+          </Popover>
+        </Fragment>
+      :
+        <Button text="Login" rightIcon="log-in" />
+      }
+    </Fragment>
+  )
+};
+
+export default UserMenu;

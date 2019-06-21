2a973a68ba545131b77f656fa36d7bec744b4dfd
AlexandrChebotar <44335524+AlexandrChebotar@users.noreply.github.com>
2019-06-21 13:17:33 +0300

    Merge from dev

---
+++ src/components/PageContainer/PageMenu/index.js
@@ -0,0 +1,48 @@
+import React, {Fragment} from 'react';
+import {
+  Navbar,
+  Switch,
+  Tabs,
+  Tab,
+} from "@blueprintjs/core";
+
+import './style.scss';
+
+const PageMenu = ({pageName, results, handleTabChange, activeTabId}) => {
+  return (
+    <div className="header">
+      <Navbar 
+      // className="bp3-dark"
+      >
+        <Navbar.Group align="left">
+          <Navbar.Heading>{pageName}</Navbar.Heading>
+        </Navbar.Group>
+        <Navbar.Group align="right">
+          <Tabs
+            animate
+            id="pageMenu"
+            selectedTabId={activeTabId}
+            onChange={handleTabChange}
+          >
+            <Tab id="players" title="Senior Players" />
+            <Tab id="traning" title="Traning" />
+            <Tab id="tactics" title="Tactics" />
+          </Tabs>
+          {results &&
+            <Fragment>
+              <Navbar.Divider />
+              <Switch label="Show results" inline/>
+            </Fragment>
+          }
+        </Navbar.Group>
+      </Navbar>
+    </div>
+  );
+};
+
+PageMenu.defaultProps = {
+  pageName: 'Squad',
+  results: true,
+}
+
+export default PageMenu;
---
+++ src/components/PageContainer/index.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import PageMenu from './PageMenu';
+
+import './style.scss';
+
+const mapStateToProps = ({user, currentClub}) => {
+  return {user, currentClub};
+};
+
+class PageContainer extends Component {
+  state = {
+    activeTabId: this.props.activeTabId,
+  };
+
+  handleTabChange = (tabId) => {
+    this.setState({activeTabId: tabId});
+  };
+
+  render() {
+    const {children, ...restProps} = this.props;
+    const {activeTabId} = this.state;
+    return (
+      <div className="page-container">
+        <div className="header">
+          <PageMenu className="bp3-dark" handleTabChange={this.handleTabChange} activeTabId={activeTabId} />
+        </div>
+        <div className="page-content">
+          {React.cloneElement(children, {...restProps, activeTabId})}
+        </div>
+      </div>
+    );
+  };
+};
+
+export default connect(mapStateToProps)(PageContainer);

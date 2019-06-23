d24f77fe5c2a3121f110055bde29e55b8a480f4c
AlexandrChebotar <44335524+AlexandrChebotar@users.noreply.github.com>
2019-06-24 00:43:54 +0300

    Add players table base functionality & styling

---
+++ src/components/PlayersTable/index.js
@@ -0,0 +1,170 @@
+import React, {Component} from 'react';
+import { ReactTabulator } from 'react-tabulator';
+
+import 'react-tabulator/lib/styles.css';
+import 'react-tabulator/lib/css/tabulator.min.css';
+import './style.scss';
+
+class PlayersTable extends Component {
+
+  getfilteredPlayers(filter) {
+    return (filter === 'forwards') ?
+      this.props.data.filter(({position}) => ['ST','FW'].includes(position)) :
+      (filter === 'midlefielders') ?
+        this.props.data.filter(({position}) => ['LW','OM','RW','LM','CM','RM','LWB','DM','RWB'].includes(position)) :
+        (filter === 'defenders') ?
+        this.props.data.filter(({position}) => position === 'CB') :
+          this.props.data;
+  };
+
+  denomFormatter(cell, {type, value = cell.getValue()}) {
+    let className = '';
+    let arrow = '';
+    if (type === 'age') {
+      className = (value < 22) ?
+        'denom4' :
+        (value < 25) ?
+          'denom6' :
+          (value < 28) ?
+            'denom7' :
+            (value < 30) ?
+              'denom8' :
+              (value < 33) ?
+                'denom3' :
+                (value < 35) ?
+                  'denom2' :
+                  'denom1';
+    } else if (type === 'rating') {
+      className = (value === 1) ?
+      'denom1' :
+      (value === 2) ?
+        'denom2':
+        (value === 3) ?
+          'denom3':
+          (value === 4) ?
+            'denom4':
+            (value === 5) ?
+              'denom5':
+              (value === 6) ?
+                'denom6':
+                (value === 7) ?
+                  'denom7':
+                  (value === 8) ?
+                    'denom8':
+                    'denom9';
+    } else if (type === 'short') {
+      className = (value === 'awf') ?
+      'denom1' :
+      (value === 'poor') ?
+        'denom2':
+        (value === 'weak') ?
+          'denom3':
+          (value === 'dec') ?
+            'denom4':
+            (value === 'good') ?
+              'denom5':
+              (value === 'exe') ?
+                'denom6':
+                (value === 'sup') ?
+                  'denom7':
+                  (value === 'bri') ?
+                    'denom8':
+                    'denom9';
+    } else  {
+      className = (value < 15) ?
+        'denom1' :
+        (value < 30) ?
+          'denom2':
+          (value < 40) ?
+            'denom3':
+            (value < 50) ?
+              'denom4':
+              (value < 60) ?
+                'denom5':
+                (value < 70) ?
+                  'denom6':
+                  (value < 80) ?
+                    'denom7':
+                    (value < 90) ?
+                      'denom8':
+                      'denom9';
+    }
+    if (type === 'form') {
+      value = (value === 15) ?
+      'awf' :
+      (value === 25) ?
+        'poor':
+        (value === 35) ?
+          'weak':
+          (value === 45) ?
+            'dec':
+            (value === 55) ?
+              'good':
+              (value === 65) ?
+                'exe':
+                (value === 75) ?
+                  'sup':
+                  (value === 85) ?
+                    'bri':
+                    (value === 95) ?
+                    'awe':
+                    value;
+      const form_rising = cell.getData().form_rising;
+      arrow = form_rising ?
+        '<div class="arrow-up"></div>' :
+        '<div class="arrow-down"></div>';
+    }
+    return `<span class="${className}">${value}${arrow}</span>`;
+  };
+
+  // experianceFormatter(cell) {
+  //   const row = cell.getData();
+  //   let {experience, experience_team: teamExperience} = row;
+  //   experience = this.denomFormatter(null, {value: experience});
+  //   teamExperience = this.denomFormatter(null, {value: teamExperience});
+  //   return `${teamExperience} / ${experience}`;
+  // };
+
+  render() {
+    const {denomFormatter, props: {filter}} = this;
+    return (
+      <ReactTabulator
+        columns={[
+          {rowHandle:true, formatter:"handle", headerSort:false, resizable:false, frozen:true, width:30},
+          {field:"flag", headerSort: false, resizable:false, formatter:"image", formatterParams:{width:"24px",height:"16px"}, tooltip: (cell) => cell.getData().nation, width: 33},
+          {title:"Name", headerTooltip: true, field:"name", widthGrow:10},
+          {title:"Age", headerTooltip: true, field:"age", formatter:denomFormatter, formatterParams:{type:"age"}, width: 40},
+          {title:"Pos", headerTooltip:"Position", field:"position", width: 45},
+          {title:"Fit", headerTooltip:"Fitness", field:"Fit", formatter:denomFormatter, headerSortStartingDir:"desc",  width: 40},
+          {title:"Exp", headerTooltip:"Experiance", field:"experience", formatter:denomFormatter, headerSortStartingDir:"desc",  width: 40},
+          {title:"T.Exp", headerTooltip:"Team Experiance", field:"experience_team", formatter:denomFormatter, headerSortStartingDir:"desc",  width: 40},
+          {title:"Form", headerTooltip:"Form", field:"form", formatter:denomFormatter, headerSortStartingDir:"desc",  formatterParams:{type:"form"}, width: 75},
+          {title:"R", headerTooltip:"Rating", field:"rating", formatter:denomFormatter, headerSortStartingDir:"desc",  formatterParams:{type:"rating"}, width:28},
+          {title:"P", headerTooltip:"Potential", field:"potential", formatter:denomFormatter, headerSortStartingDir:"desc",  formatterParams:{type:"rating"}, width:28},
+          {title:"SC", headerTooltip:"Score", field:"SC", formatter:denomFormatter, headerSortStartingDir:"desc",  width: 40},
+          {title:"OP", headerTooltip:"Offencive Position", field:"OP", formatter:denomFormatter, headerSortStartingDir:"desc",  width: 40},
+          {title:"BC", headerTooltip:"Ball Control", field:"BC", formatter:denomFormatter, headerSortStartingDir:"desc",  width: 40},
+          {title:"PA", headerTooltip:"Passes", field:"PA", formatter:denomFormatter, headerSortStartingDir:"desc",  width: 40},
+          {title:"AE", headerTooltip:"Aerial", field:"AE", formatter:denomFormatter, headerSortStartingDir:"desc",  width: 40},
+          {title:"CO", headerTooltip:"Constitution", field:"CO", formatter:denomFormatter, headerSortStartingDir:"desc",  width: 40},
+          {title:"TA", headerTooltip:"Tackles", field:"TA", formatter:denomFormatter, headerSortStartingDir:"desc",  width: 40},
+          {title:"DP", headerTooltip:"Defence Position" ,field:"DP", formatter:denomFormatter, headerSortStartingDir:"desc", width: 40},
+        ]}
+        data={this.getfilteredPlayers(filter)}
+        options={{
+          // height:  500,
+          layout: "fitColumns",
+          movableRows: true,
+          columnMinWidth: 25,
+          pagination:"local",
+          paginationSize: 15,
+          paginationSizeSelector:[5, 10, 15, 20, 25, 50, 100],
+          // sortOrderReverse: true,
+          selectable: true,
+        }}
+      />
+    );
+  };
+};
+
+export default PlayersTable;
--- src/pages/Squad/index.js
+++ src/pages/Squad/index.js
@@ -1,196 +1,84 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PageContainer from '../../components/PageContainer';
+import PlayersTable from '../../components/PlayersTable';
+import {
+  Tabs,
+  Tab,
+  Switch,
+} from "@blueprintjs/core";
+
+import players from '../../store/players.json';
 
 import './style.scss';
 
 const Squad = ({match}) => {
   return (
     <PageContainer activeTabId={match.params.activeTabId}>
-      <Content />
+      <Content players={players} />
     </PageContainer>
   );
 }
 
-const Content = ({activeTabId}) => {
-  return (
-    <div className="content">
-      {activeTabId}
-      <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Country</th>
-                    <th>Status</th>
-                    <th>Age</th>
-                    <th>Raiting</th>
-                    <th>Exp</th>
-                    <th>Frm</th>
-                    <th>P1</th>
-                    <th>P2</th>
-                    <th>P3</th>
-                    <th>P4</th>
-                    <th>P5</th>
-                    <th>P6</th>
-                    <th>P7</th>
-                    <th>P8</th>
-                    <th>P9</th>
-                    <th>P10</th>
-                    <th>P11</th>
-                    <th>P12</th>
-                    <th>P13</th>
-                    <th>P14</th>
-                    <th>P15</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Cevin Franco</td>
-                    <td>Arg</td>
-                    <td>+</td>
-                    <td>21</td>
-                    <td>7/9</td>
-                    <td>Good</td>
-                    <td>Awe</td>
-                    <td>31</td>
-                    <td>70</td>
-                    <td>71</td>
-                    <td>45</td>
-                    <td>48</td>
-                    <td>42</td>
-                    <td>16</td>
-                    <td>28</td>
-                    <td>80</td>
-                    <td>38</td>
-                    <td>45</td>
-                    <td>47</td>
-                    <td>68</td>
-                    <td>65</td>
-                    <td>33</td>
-                </tr>
-                <tr>
-                    <td>Ingvar Strado</td>
-                    <td>Arg</td>
-                    <td>C</td>
-                    <td>30</td>
-                    <td>8/8</td>
-                    <td>Awe</td>
-                    <td>Exe</td>
-                    <td>25</td>
-                    <td>28</td>
-                    <td>78</td>
-                    <td>52</td>
-                    <td>55</td>
-                    <td>58</td>
-                    <td>68</td>
-                    <td>60</td>
-                    <td>88</td>
-                    <td>48</td>
-                    <td>42</td>
-                    <td>40</td>
-                    <td>71</td>
-                    <td>70</td>
-                    <td>63</td>
-                </tr>
-                <tr>
-                    <td>Chui Chaves</td>
-                    <td>Arg</td>
-                    <td></td>
-                    <td>25</td>
-                    <td>7/8</td>
-                    <td>Awe</td>
-                    <td>Exe</td>
-                    <td>30</td>
-                    <td>30</td>
-                    <td>32</td>
-                    <td>70</td>
-                    <td>70</td>
-                    <td>78</td>
-                    <td>72</td>
-                    <td>50</td>
-                    <td>51</td>
-                    <td>58</td>
-                    <td>38</td>
-                    <td>30</td>
-                    <td>66</td>
-                    <td>78</td>
-                    <td>62</td>
-                </tr>
-                    <tr>
-                    <td>Niko Kovach</td>
-                    <td>Cro</td>
-                    <td></td>
-                    <td>22</td>
-                    <td>7/9</td>
-                    <td>Good</td>
-                    <td>Good</td>
-                    <td>77</td>
-                    <td>33</td>
-                    <td>82</td>
-                    <td>80</td>
-                    <td>57</td>
-                    <td>50</td>
-                    <td>55</td>
-                    <td>88</td>
-                    <td>91</td>
-                    <td>72</td>
-                    <td>22</td>
-                    <td>40</td>
-                    <td>68</td>
-                    <td>78</td>
-                    <td>55</td>
-                </tr>
-                    <tr>
-                    <td>Ars Art</td>
-                    <td>Ukr</td>
-                    <td></td>
-                    <td>28</td>
-                    <td>7/7</td>
-                    <td>Exe</td>
-                    <td>Exe</td>
-                    <td>40</td>
-                    <td>40</td>
-                    <td>40</td>
-                    <td>41</td>
-                    <td>88</td>
-                    <td>82</td>
-                    <td>50</td>
-                    <td>55</td>
-                    <td>85</td>
-                    <td>52</td>
-                    <td>25</td>
-                    <td>28</td>
-                    <td>38</td>
-                    <td>18</td>
-                    <td>78</td>
-                </tr>
-                    <tr>
-                    <td>Alpha Bravo</td>
-                    <td>USA</td>
-                    <td></td>
-                    <td>28</td>
-                    <td>7/8</td>
-                    <td>Bri</td>
-                    <td>Exe</td>
-                    <td>60</td>
-                    <td>30</td>
-                    <td>60</td>
-                    <td>41</td>
-                    <td>44</td>
-                    <td>24</td>
-                    <td>80</td>
-                    <td>82</td>
-                    <td>38</td>
-                    <td>45</td>
-                    <td>44</td>
-                    <td>28</td>
-                    <td>80</td>
-                    <td>49</td>
-                    <td>90</td>
-                </tr>
-            </tbody>
-        </table>
-    </div> 
-  );
-}
+class Content extends Component {
+  state = {
+    activeTabId: 'forvards',
+  };
+
+  handleTabChange = (id) => {
+    this.setState({activeTabId: id});
+  };
+
+  getOutfielpers() {
+    return this.props.players.filter(({position}) => position !== 'GK').map(({
+      id, 
+      country_info: {title: nation, url: flag}, 
+      name, age, age_long, current_rating: rating , rating: potential,
+      experience, experience_team, foot, form_mid: form, form_rising, position, 
+      skills: {AE, BC, CO, DP, OP, PA, SC, TA, Fit}
+    }) => ({
+      id, nation, flag: `https://tacticalfootball.com/${flag}`, name, age, age_long, rating, potential, 
+      potential, experience, experience_team, foot, form, form_rising,
+      position, Fit, SC, OP, BC, PA, AE, CO, TA, DP
+    })).sort(({potential:A},{potential:B})=>B-A).sort(({rating:A},{rating:B})=>B-A).sort(({age:A},{age:B})=>B-A);
+  };
+  
+  getGoalkeepers() {
+    return this.props.players.filter(({position}) => position === 'GK').map(({
+      id, 
+      country_info: {title: nation, url: flag}, 
+      name, age, age_long, current_rating: rating , rating: potential,
+      experience, experience_team, foot, form_mid: form, form_rising, position, 
+      skills: {RE, GP, IN, CT, OR, CO, Fit}
+    }) => ({
+      id, nation, flag: `https://tacticalfootball.com/${flag}`, name, age, age_long, rating, potential, 
+      potential, experience, experience_team, foot, form, form_rising,
+      position, Fit, RE, GP, IN, CT, OR, CO
+    }));
+  };
+
+  render() {
+    const {denomFormatter} = this;
+    return (
+      <div className="content">
+        {this.props.activeTabId} => {this.state.activeTabId}
+        <Tabs
+            animate
+            id="playersTable"
+            selectedTabId={this.state.activeTabId}
+            onChange={this.handleTabChange}
+            renderActiveTabPanelOnly
+          >
+            <Tab id="outfielders" title="All Outfielders" panel={<PlayersTable data={this.getOutfielpers()} />} />
+            <Tab id="forvards" title="Forvards" panel={<PlayersTable data={this.getOutfielpers()} filter="forwards" />} />
+            <Tab id="midlefielders" title="Midlefielders" panel={<PlayersTable data={this.getOutfielpers()} filter="midlefielders" />} />
+            <Tab id="defenders" title="Defenders" panel={<PlayersTable data={this.getOutfielpers()} filter="defenders" />} />
+            <Tab id="goalkeepers" title="Goalkeepers" panel={<PlayersTable data={this.getGoalkeepers()} />} />
+            <Tabs.Expander />
+            <Switch label="Sort by potentials" inline/>
+          </Tabs>
+      </div> 
+    );
+  };
+};
 
 export default Squad;

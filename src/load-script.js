3dda853d7f9a9a59b1004641348867db100d80c8
AlexandrChebotar <44335524+AlexandrChebotar@users.noreply.github.com>
2019-07-01 22:26:21 +0300

    Remove use of DOMParser

--- src/load-script.js
+++ src/load-script.js
@@ -5,6 +5,7 @@
 // @grant none
 // ==/UserScript==
 
+// redefining setInterval & setTimeout
 let _timers = [];
 window.setIntervalBase = window.setInterval;
 window.setInterval = function(callback, interval) {
@@ -21,6 +22,7 @@ window.setTimeout = function(callback, interval) {
 const removeAllTimers = () => {
   _timers.forEach(({timerID}) => clearInterval(timerID));
 };
+// redefining addEventListener
 let _listeners = [];
 window.addEventListenerBase = window.addEventListener;
 document.addEventListenerBase = document.addEventListener;
@@ -33,6 +35,7 @@ document.addEventListener = addEventListener;
 const removeAllEventListeners = function() {
   _listeners.forEach(({target, type, listener}) => target.removeEventListener(type, listener));
 };
+// redefining console
 let _console = [];
 window._console = _console;
 window.console.logBase = window.console.log;
@@ -40,7 +43,7 @@ window.console.log = function() {
   _console.push(arguments);
   window.console.logBase.apply(window.console, arguments);
 };
-const getSubmenu = () => {
+const getMenus = () => {
   document.querySelector('.rb-nav_arrow').parentNode.click();
   navbarMenu = _console.find(item => item[0].sub_menu && item[0].sub_menu.find(item => item.sub_menu && item.sub_menu[0].text==='Current'))[0].sub_menu;
   window._competitionsMenu = navbarMenu[5].sub_menu;
@@ -77,25 +80,30 @@ window.onload = () => {
   menu.appendChild(menuItem);
   
   function loadAltUI() {
+    // get competitions and forums menus
+    getMenus();
+    // remove old data
     removeAllEventListeners();
     removeAllTimers();
-    getSubmenu();
     const html = document.querySelector('html');
     const {head, body} = document;
     html.removeAttribute('ng-app');
     html.removeAttribute('class');
     body.removeAttribute('ng-controller');
     body.removeAttribute('class');
-    const parser = new DOMParser();
-    // index.html
-    const indexHTML = '<!doctype html><html lang="en"><head><meta charset="utf-8"/><link rel="shortcut icon" href="/favicon.ico"/><meta name="viewport" content="width=device-width,initial-scale=1"/><meta name="theme-color" content="#000000"/><link rel="manifest" href="/manifest.json"/><title>React App</title><link href="/static/css/2.f5d5d3a0.chunk.css" rel="stylesheet"><link href="/static/css/main.63f034f0.chunk.css" rel="stylesheet"></head><body><noscript>You need to enable JavaScript to run this app.</noscript><div id="root"></div><script>!function(l){function e(e){for(var r,t,n=e[0],o=e[1],u=e[2],f=0,i=[];f<n.length;f++)t=n[f],p[t]&&i.push(p[t][0]),p[t]=0;for(r in o)Object.prototype.hasOwnProperty.call(o,r)&&(l[r]=o[r]);for(s&&s(e);i.length;)i.shift()();return c.push.apply(c,u||[]),a()}function a(){for(var e,r=0;r<c.length;r++){for(var t=c[r],n=!0,o=1;o<t.length;o++){var u=t[o];0!==p[u]&&(n=!1)}n&&(c.splice(r--,1),e=f(f.s=t[0]))}return e}var t={},p={1:0},c=[];function f(e){if(t[e])return t[e].exports;var r=t[e]={i:e,l:!1,exports:{}};return l[e].call(r.exports,r,r.exports,f),r.l=!0,r.exports}f.m=l,f.c=t,f.d=function(e,r,t){f.o(e,r)||Object.defineProperty(e,r,{enumerable:!0,get:t})},f.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},f.t=function(r,e){if(1&e&&(r=f(r)),8&e)return r;if(4&e&&"object"==typeof r&&r&&r.__esModule)return r;var t=Object.create(null);if(f.r(t),Object.defineProperty(t,"default",{enumerable:!0,value:r}),2&e&&"string"!=typeof r)for(var n in r)f.d(t,n,function(e){return r[e]}.bind(null,n));return t},f.n=function(e){var r=e&&e.__esModule?function(){return e.default}:function(){return e};return f.d(r,"a",r),r},f.o=function(e,r){return Object.prototype.hasOwnProperty.call(e,r)},f.p="/";var r=window.webpackJsonp=window.webpackJsonp||[],n=r.push.bind(r);r.push=e,r=r.slice();for(var o=0;o<r.length;o++)e(r[o]);var s=n;a()}([])</script><script src="/static/js/2.d99691d1.chunk.js"></script><script src="/static/js/main.4acbb867.chunk.js"></script></body></html>'
-    const altDocument = parser.parseFromString(indexHTML, "text/html");
-    window.altDocument = altDocument;
-    history.pushState({}, "TF Alt UI", "/");
-    // CSS
-    const linkElements = [...altDocument.head.getElementsByTagName('link')];
-    linkElements.forEach(link => altDocument.head.removeChild(link));
-    head.innerHTML = altDocument.head.innerHTML;
+    // build new DOM structure
+    const headContent = `
+      <meta charset="utf-8"/>
+      <link rel="shortcut icon" href="/favicon.ico"/>
+      <meta name="viewport" content="width=device-width,initial-scale=1"/>
+      <meta name="theme-color" content="#000000"/>
+      <link rel="manifest" href="/manifest.json"/>
+      <title>TF Alt UI</title>`;
+    head.innerHTML = headContent;
+    const bodyContent = '<div id="root"></div>';
+    body.innerHTML = bodyContent;
+    history.pushState(null, "TF Alt UI", "/");
+    // load CSS
     const css1 = document.createElement('link');
     css1.rel = 'stylesheet';
     css1.href = 'https://cdn.jsdelivr.net/gh/AlexandrChebotar/tacticalfootball-alt-ui@gh-pages/static/css/2.f5d5d3a0.chunk.css';
@@ -104,10 +112,7 @@ window.onload = () => {
     css2.rel = 'stylesheet';
     css2.href = 'https://cdn.jsdelivr.net/gh/AlexandrChebotar/tacticalfootball-alt-ui@gh-pages/static/css/main.63f034f0.chunk.css';
     head.appendChild(css2);
-    // JS
-    const scriptElements = [...altDocument.body.getElementsByTagName('script')];
-    scriptElements.forEach((script, index) => altDocument.body.removeChild(script));
-    body.innerHTML = altDocument.body.innerHTML;
+    // load JS
     const js1 = document.createElement('script');
     js1.src = 'http://localhost:5000/static/js/_1.js';
     body.appendChild(js1);

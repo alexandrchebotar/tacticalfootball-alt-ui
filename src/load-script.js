5b99d5bd2c99916cc62a7f071712f233aaa8be5b
AlexandrChebotar <44335524+AlexandrChebotar@users.noreply.github.com>
2019-07-01 21:58:04 +0300

    Add removing old timers

--- src/load-script.js
+++ src/load-script.js
@@ -5,9 +5,23 @@
 // @grant none
 // ==/UserScript==
 
+let _timers = [];
+window.setIntervalBase = window.setInterval;
+window.setInterval = function(callback, interval) {
+  const timerID = window.setIntervalBase(callback, interval);
+  _timers.push({timerID, callback, interval});
+  return timerID;
+};
+window.setTimeoutBase = window.setTimeout;
+window.setTimeout = function(callback, interval) {
+  const timerID = window.setTimeoutBase(callback, interval);
+  _timers.push({timerID, callback, interval});
+  return timerID;
+};
+const removeAllTimers = () => {
+  _timers.forEach(({timerID}) => clearInterval(timerID));
+};
 let _listeners = [];
-window._l = _listeners;
-debugger;
 window.addEventListenerBase = window.addEventListener;
 document.addEventListenerBase = document.addEventListener;
 const addEventListener = function(type, listener) {
@@ -16,19 +30,9 @@ const addEventListener = function(type, listener) {
 };
 window.addEventListener = addEventListener;
 document.addEventListener = addEventListener;
-const removeEventListeners = function(targetType) {
-  _listeners.forEach(({target, type, listener}) => {
-    if(target == this && type == targetType) {
-      this.removeEventListener(type, listener);
-    }     
-  });
-};
 const removeAllEventListeners = function() {
-  _listeners.forEach(({target, type, listener}) => this.removeEventListener(type, listener));
+  _listeners.forEach(({target, type, listener}) => target.removeEventListener(type, listener));
 };
-window.removeAllEventListeners = removeAllEventListeners;
-window.removeEventListeners = removeEventListeners;
-document.removeEventListeners = removeEventListeners;
 
 window.onload = () => {
   const menuItem = document.createElement('li');
@@ -61,6 +65,13 @@ window.onload = () => {
   
   function loadAltUI() {
     removeAllEventListeners();
+    removeAllTimers();
+    const html = document.querySelector('html');
+    const {head, body} = document;
+    html.removeAttribute('ng-app');
+    html.removeAttribute('class');
+    body.removeAttribute('ng-controller');
+    body.removeAttribute('class');
     const parser = new DOMParser();
     // index.html
     const indexHTML = '<!doctype html><html lang="en"><head><meta charset="utf-8"/><link rel="shortcut icon" href="/favicon.ico"/><meta name="viewport" content="width=device-width,initial-scale=1"/><meta name="theme-color" content="#000000"/><link rel="manifest" href="/manifest.json"/><title>React App</title><link href="/static/css/2.f5d5d3a0.chunk.css" rel="stylesheet"><link href="/static/css/main.63f034f0.chunk.css" rel="stylesheet"></head><body><noscript>You need to enable JavaScript to run this app.</noscript><div id="root"></div><script>!function(l){function e(e){for(var r,t,n=e[0],o=e[1],u=e[2],f=0,i=[];f<n.length;f++)t=n[f],p[t]&&i.push(p[t][0]),p[t]=0;for(r in o)Object.prototype.hasOwnProperty.call(o,r)&&(l[r]=o[r]);for(s&&s(e);i.length;)i.shift()();return c.push.apply(c,u||[]),a()}function a(){for(var e,r=0;r<c.length;r++){for(var t=c[r],n=!0,o=1;o<t.length;o++){var u=t[o];0!==p[u]&&(n=!1)}n&&(c.splice(r--,1),e=f(f.s=t[0]))}return e}var t={},p={1:0},c=[];function f(e){if(t[e])return t[e].exports;var r=t[e]={i:e,l:!1,exports:{}};return l[e].call(r.exports,r,r.exports,f),r.l=!0,r.exports}f.m=l,f.c=t,f.d=function(e,r,t){f.o(e,r)||Object.defineProperty(e,r,{enumerable:!0,get:t})},f.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},f.t=function(r,e){if(1&e&&(r=f(r)),8&e)return r;if(4&e&&"object"==typeof r&&r&&r.__esModule)return r;var t=Object.create(null);if(f.r(t),Object.defineProperty(t,"default",{enumerable:!0,value:r}),2&e&&"string"!=typeof r)for(var n in r)f.d(t,n,function(e){return r[e]}.bind(null,n));return t},f.n=function(e){var r=e&&e.__esModule?function(){return e.default}:function(){return e};return f.d(r,"a",r),r},f.o=function(e,r){return Object.prototype.hasOwnProperty.call(e,r)},f.p="/";var r=window.webpackJsonp=window.webpackJsonp||[],n=r.push.bind(r);r.push=e,r=r.slice();for(var o=0;o<r.length;o++)e(r[o]);var s=n;a()}([])</script><script src="/static/js/2.d99691d1.chunk.js"></script><script src="/static/js/main.4acbb867.chunk.js"></script></body></html>'
@@ -70,27 +81,27 @@ window.onload = () => {
     // CSS
     const linkElements = [...altDocument.head.getElementsByTagName('link')];
     linkElements.forEach(link => altDocument.head.removeChild(link));
-    document.head.innerHTML = altDocument.head.innerHTML;
+    head.innerHTML = altDocument.head.innerHTML;
     const css1 = document.createElement('link');
     css1.rel = 'stylesheet';
     css1.href = 'https://cdn.jsdelivr.net/gh/AlexandrChebotar/tacticalfootball-alt-ui@gh-pages/static/css/2.f5d5d3a0.chunk.css';
-    document.head.appendChild(css1);
+    head.appendChild(css1);
     const css2 = document.createElement('link');
     css2.rel = 'stylesheet';
     css2.href = 'https://cdn.jsdelivr.net/gh/AlexandrChebotar/tacticalfootball-alt-ui@gh-pages/static/css/main.63f034f0.chunk.css';
-    document.head.appendChild(css2);
+    head.appendChild(css2);
     // JS
     const scriptElements = [...altDocument.body.getElementsByTagName('script')];
     scriptElements.forEach((script, index) => altDocument.body.removeChild(script));
-    document.body.innerHTML = altDocument.body.innerHTML;
+    body.innerHTML = altDocument.body.innerHTML;
     const js1 = document.createElement('script');
     js1.src = 'http://localhost:5000/static/js/_1.js';
-    document.body.appendChild(js1);
+    body.appendChild(js1);
     const js2 = document.createElement('script');
     js2.src = 'http://localhost:5000/static/js/2.js';
-    document.body.appendChild(js2);
+    body.appendChild(js2);
     const js3 = document.createElement('script');
     js3.src = 'http://localhost:5000/static/js/3.js';
-    document.body.appendChild(js3);
+    body.appendChild(js3);
   }
 };

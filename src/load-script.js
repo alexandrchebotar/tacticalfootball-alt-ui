5b62d7699542e9f5cccdb85c97f67473f0afa25d
AlexandrChebotar <44335524+AlexandrChebotar@users.noreply.github.com>
2019-07-02 13:22:28 +0300

    Add load react app without loading TacticalFootballApp

--- src/load-script.js
+++ src/load-script.js
@@ -4,7 +4,11 @@
 // @match https://tacticalfootball.com/*
 // @grant none
 // ==/UserScript==
-
+debugger;
+// load AltUI if location contains hash
+if (window.location.hash) {
+  clearTacticalFootballApp()
+}
 // redefining setInterval & setTimeout
 let _timers = [];
 window.setIntervalBase = window.setInterval;
@@ -51,76 +55,89 @@ const getMenus = () => {
 };
 
 window.onload = () => {
-  const menuItem = document.createElement('li');
-  menuItem.className = 'ng-scope';
-  menuItem.innerHTML = `
-    <div class="any ng-scope">
-      <div class="rb-nav-menu-item ng-scope" >
-        <span class="ng-scope ng-binding menuSectionHdr">
-          Alternate UI
-        </span>
-      </div>
-    </div>
-    <div class="rb-nav_sub-menu ng-scope dropdown-animate-left" >
-      <ul class="ng-scope">
-       <li class="ng-scope">
-        <div class="any ng-scope">
-          <a class="rb-nav-menu-item ng-scope load-alt-ui" href="#" >
-            <span class="ng-scope ng-binding">
-              Load
-            </span>
-          </a>
+  if (window.location.hash) {
+    loadAltUI();
+  } else {
+    const menuItem = document.createElement('li');
+    menuItem.className = 'ng-scope';
+    menuItem.innerHTML = `
+      <div class="any ng-scope">
+        <div class="rb-nav-menu-item ng-scope" >
+          <span class="ng-scope ng-binding menuSectionHdr">
+            Alternate UI
+          </span>
         </div>
-       </li>
-      </ul>
-    </div>`;
-  const loadAnchor = menuItem.querySelector('.load-alt-ui');
-  loadAnchor.addEventListener('click', loadAltUI);
-  const menu = document.querySelector(`.rb-nav_menu ul[ng-include="'rbMenu/rbMenuLine.html'"]`);
-  menu.appendChild(menuItem);
-  
-  function loadAltUI() {
-    // get competitions and forums menus
-    getMenus();
-    // remove old data
-    removeAllEventListeners();
-    removeAllTimers();
-    const html = document.querySelector('html');
-    const {head, body} = document;
-    html.removeAttribute('ng-app');
-    html.removeAttribute('class');
-    body.removeAttribute('ng-controller');
-    body.removeAttribute('class');
-    // build new DOM structure
-    const headContent = `
-      <meta charset="utf-8"/>
-      <link rel="shortcut icon" href="/favicon.ico"/>
-      <meta name="viewport" content="width=device-width,initial-scale=1"/>
-      <meta name="theme-color" content="#000000"/>
-      <link rel="manifest" href="/manifest.json"/>
-      <title>TF Alt UI</title>`;
-    head.innerHTML = headContent;
-    const bodyContent = '<div id="root"></div>';
-    body.innerHTML = bodyContent;
-    history.pushState(null, "TF Alt UI", "/");
-    // load CSS
-    const css1 = document.createElement('link');
-    css1.rel = 'stylesheet';
-    css1.href = 'https://cdn.jsdelivr.net/gh/AlexandrChebotar/tacticalfootball-alt-ui/static/css/1.css';
-    head.appendChild(css1);
-    const css2 = document.createElement('link');
-    css2.rel = 'stylesheet';
-    css2.href = 'https://cdn.jsdelivr.net/gh/AlexandrChebotar/tacticalfootball-alt-ui/static/css/2.css';
-    head.appendChild(css2);
-    // load JS
-    const js1 = document.createElement('script');
-    js1.src = 'https://cdn.jsdelivr.net/gh/AlexandrChebotar/tacticalfootball-alt-ui/static/js/1.js';
-    body.appendChild(js1);
-    const js2 = document.createElement('script');
-    js2.src = 'https://cdn.jsdelivr.net/gh/AlexandrChebotar/tacticalfootball-alt-ui/static/js/2.js';
-    body.appendChild(js2);
-    const js3 = document.createElement('script');
-    js3.src = 'https://cdn.jsdelivr.net/gh/AlexandrChebotar/tacticalfootball-alt-ui/static/js/3.js';
-    body.appendChild(js3);
+      </div>
+      <div class="rb-nav_sub-menu ng-scope dropdown-animate-left" >
+        <ul class="ng-scope">
+         <li class="ng-scope">
+          <div class="any ng-scope">
+            <a class="rb-nav-menu-item ng-scope load-alt-ui" href="#" >
+              <span class="ng-scope ng-binding">
+                Load
+              </span>
+            </a>
+          </div>
+         </li>
+        </ul>
+      </div>`;
+    const loadAnchor = menuItem.querySelector('.load-alt-ui');
+    loadAnchor.addEventListener('click', loadAltUI);
+    const menu = document.querySelector(`.rb-nav_menu ul[ng-include="'rbMenu/rbMenuLine.html'"]`);
+    menu.appendChild(menuItem);
   }
 };
+
+function clearTacticalFootballApp() {
+  document.querySelector('html').removeAttribute('ng-app');
+  document.body.innerHTML = '';
+}
+
+function loadAltUI() {
+  debugger;
+  // get competitions and forums menus
+  if (!window.location.hash) {
+    getMenus();
+  }
+  // remove old data
+  removeAllEventListeners();
+  removeAllTimers();
+  // remove old DOM data
+  const html = document.querySelector('html');
+  const {head, body} = document;
+  html.removeAttribute('ng-app');
+  html.removeAttribute('class');
+  body.removeAttribute('ng-controller');
+  body.removeAttribute('class');
+  // build new DOM structure
+  const headContent = `
+    <meta charset="utf-8"/>
+    <link rel="shortcut icon" href="/favicon.ico"/>
+    <meta name="viewport" content="width=device-width,initial-scale=1"/>
+    <meta name="theme-color" content="#000000"/>
+    <link rel="manifest" href="/manifest.json"/>
+    <title>TF Alt UI</title>`;
+  head.innerHTML = headContent;
+  const bodyContent = '<div id="root"></div>';
+  body.innerHTML = bodyContent;
+  history.pushState(null, "TF Alt UI", "/");
+  // load CSS
+  const css1 = document.createElement('link');
+  css1.rel = 'stylesheet';
+  css1.href = 'https://cdn.jsdelivr.net/gh/AlexandrChebotar/tacticalfootball-alt-ui@0.1.1/dist/static/css/1.css';
+  head.appendChild(css1);
+  const css2 = document.createElement('link');
+  css2.rel = 'stylesheet';
+  css2.href = 'https://cdn.jsdelivr.net/gh/AlexandrChebotar/tacticalfootball-alt-ui@0.1.1/dist/static/css/2.css';
+  head.appendChild(css2);
+  // load JS
+  const js1 = document.createElement('script');
+  js1.src = 'https://cdn.jsdelivr.net/gh/AlexandrChebotar/tacticalfootball-alt-ui@0.1.1/dist/static/js/1.js';
+  body.appendChild(js1);
+  const js2 = document.createElement('script');
+  js2.src = 'https://cdn.jsdelivr.net/gh/AlexandrChebotar/tacticalfootball-alt-ui@0.1.1/dist/static/js/2.js';
+  body.appendChild(js2);
+  const js3 = document.createElement('script');
+  js3.src = 'https://cdn.jsdelivr.net/gh/AlexandrChebotar/tacticalfootball-alt-ui@0.1.1/dist/static/js/3.js';
+  body.appendChild(js3);
+  }
